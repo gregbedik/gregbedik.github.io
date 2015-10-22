218309841fb0660963f6578a512ab7a3e89aa90e
Greg Bedik <gbedik@albany.edu>
2015-10-21 21:37:53 -0400

    Delete javascript.js

--- js/javascript.js
+++
@@ -1,8 +0,0 @@
-var numChildren = 2;
-var partnerName = 'Blake Lively';
-var geoLocation = 'New York';
-var jobTitle = 'Front End Designer';
-
-var fortune = 'You will be a ' +  jobTitle + ' in ' + geoLocation + ', and married to ' + partnerName + 'with ' + numChildren + ' kids.';
-
-console.log(fortune);
