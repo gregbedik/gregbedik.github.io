b6dd2d9f85067ea968cc02a18cd9f0da425e2f91
Greg Bedik <gbedik@albany.edu>
2015-10-21 21:57:38 -0400

    Update and rename javascript.html to javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -3,6 +3,6 @@ var partnerName = 'Blake Lively';
 var geoLocation = 'New York';
 var jobTitle = 'Front End Designer';
 
-var fortune = 'You will be a ' +  jobTitle + ' in ' + geoLocation + ', and married to ' + partnerName + 'with ' + numChildren + ' kids.';
+var fortune = 'You will be a ' +  jobTitle + ' in ' + geoLocation + ', and married to ' + partnerName + ' with ' + numChildren + ' kids.';
 
 console.log(fortune);
